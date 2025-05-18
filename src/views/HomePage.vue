596a7e1118370c2831a9f808b7b46151286e32a3
Bayu Rizky Nurseto <bayu.nurseto@ottencoffee.co.id>
2025-05-18 12:34:15 +0700

    first ionic vue project

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,55 +2,93 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Tugas 2 - Ionic Vue</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <div class="btn-action">
+          <ion-button @click="fetchData">Get Data</ion-button>
+        </div>
+
+        <div class="wrapper-table">
+          <ion-grid>
+            <ion-row class="head-table">
+              <ion-col>Name</ion-col>
+              <ion-col>Symbol</ion-col>
+              <ion-col>Harga USD</ion-col>
+            </ion-row>
+            <ion-row v-for="currency in dataCurrencyArr" :key="currency.id">
+              <ion-col>{{ currency.name }}</ion-col>
+              <ion-col>{{ currency.symbol }}</ion-col>
+              <ion-col>{{ currency.price_usd }}</ion-col>
+            </ion-row>
+          </ion-grid>
+        </div>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script>
+export default {
+  data() {
+    return {
+      dataCurrencyArr: [],
+    };
+  },
+  methods: {
+    fetchData() {
+      fetch('https://api.coinlore.net/api/tickers/', {
+        method: 'GET',
+      })
+        .then((res) => {
+          res.json().then((data) => {
+            this.dataCurrencyArr = data.data.slice(0, 10);
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
 #container {
+  font-size: 14px;
+}
+
+.btn-action {
+  color: white;
   text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+  margin-top: 16px;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+.wrapper-table {
+  margin-top: 16px;
+  padding: 4px;
 }
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
+ion-grid {
+  border: 1px solid black;
+  padding: 0;
 }
 
-#container a {
-  text-decoration: none;
+ion-row {
+  width: 100%;
+  height: auto;
+  text-align: center;
+  align-items: center;
+}
+
+ion-row.head-table {
+  border-bottom: 1px solid;
+}
+
+ion-col {
+  padding: 8px;
 }
 </style>
